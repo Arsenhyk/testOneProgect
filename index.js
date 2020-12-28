94fbc0806f30249465fa4557cee4073b3204e2ea
arsenhuk_oleh <aresnhykoleh@gmail.com>
2020-12-29 00:10:23 +0200

    corrected render

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const mongoose  = require('mongoose');
-const exphds = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const todoRoutes = require('./routes/todos');
 
 const PORT = process.env.PORT || 8081
 
 const app = express()
-const hbs = exphds.create({
+const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
 });
 
 app.engine('hbs', hbs.engine)
-app.set('view engie', 'hbs')
+app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(todoRoutes);
